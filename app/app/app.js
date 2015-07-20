5d8a385bf3f37d88b0c91aa9f6278b6b78bb53a1
Lucas Myers <lucas@unthunk.com>
2015-07-20 01:25:49 -0400

    include firebase in app

---
+++ app/app/app.js
@@ -0,0 +1,7 @@
+var firebase = '';
+
+// create a firebase reference
+var ref = new Firebase(firebase);
+
+// we can now interact with the firebase db through ref
+// var usersRef = ref.child("users");
