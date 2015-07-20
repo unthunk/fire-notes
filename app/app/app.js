2cbd75fc98fcb2f7605ec3c09f0d6d4a2b246214
Lucas Myers <lucas@unthunk.com>
2015-07-20 15:58:09 -0400

    adding user reg and auth

--- app/app/app.js
+++ app/app/app.js
@@ -1,11 +1,83 @@
-// declare what firebase db we want to use e.g. https://fire-foo-1234.firebaseio.com/
+// declare what firebase db we want to use e.g. https://<YOUR-FIREBASE-APP>.firebaseio.com
 var firebase = config.firebase;
 
 // create a firebase reference
 var ref = new Firebase(firebase);
 
 // we can now interact with the firebase db through ref
-var notesRef = ref.child('notes');
+var userRef = ref.child('users');
+
+
+// form elements
+var loginForm = document.querySelector('.login'),
+  loginButton = document.querySelector('.login-button'),
+  registerLink = document.querySelector('.register-link');
+
+
+// add bindings to form elements
+loginForm.addEventListener('submit',function(e){
+  e.preventDefault();
+  console.log('subitted form');
+
+  login(email(), password());
+});
+
+registerLink.addEventListener('click',function(e){
+  e.preventDefault();
+  console.log('register button');
+
+  register(email(), password());
+});
+
+
+// register users
+function register(email, password) {
+  ref.createUser({
+    email: email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
+}
+
+
+// login using email/password
+function login(email, password) {
+  ref.authWithPassword({
+    email: email,
+    password: password
+  }, authHandler);
+}
+
+
+// callback for after auth
+function authHandler(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+}
+
+
+// return email address
+function email() {
+  return document.querySelector('#email').value;
+}
+
+
+// return password
+function password() {
+  return document.querySelector('#password').value;
+}
+
+
+
+// we need to rethink how we handle notes now that we have users
 
 // get our note element
 var el = document.querySelectorAll('.note-content')[0];
