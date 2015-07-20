c11edf1ad2237efef6e2a162741bab7e9ed0279f
Lucas Myers <lucas@unthunk.com>
2015-07-20 11:30:25 -0400

    add a simple note

--- app/app/app.js
+++ app/app/app.js
@@ -1,7 +1,32 @@
-var firebase = '';
+// declare what firebase db we want to use e.g. https://fire-foo-1234.firebaseio.com/
+var firebase = config.firebase;
 
 // create a firebase reference
 var ref = new Firebase(firebase);
 
 // we can now interact with the firebase db through ref
-// var usersRef = ref.child("users");
+var notesRef = ref.child('notes');
+
+// get our note element
+var el = document.querySelectorAll('.note-content')[0];
+
+// add an event listener to update firebase when the element's content changes
+el.addEventListener('input',function(){
+  console.log('the note changed', this);
+
+  // get the note name and content
+  var content = this.innerText;
+    name = this.getAttribute('data-note-name');
+
+  // call the update fn
+  updateNote(name,content);
+
+});
+
+function updateNote(name,content) {
+  // create a reference to the note we want to change, if it doesn't already
+  // exist firebase will create a new node for it when we save or update it
+  var note = notesRef.child(name);
+
+  note.update({'content': content});
+}
