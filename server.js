431bc6be14a6011d87166e692082fac511a02928
Lucas Myers <lucas@unthunk.com>
2015-07-20 11:26:22 -0400

    adding a config file to store firebase URL, etc

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 
 // serve bower_components from correct directory
 app.use('/bower_components',express.static(__dirname + '/bower_components'));
+
+// serve config file for demo purposes
+app.use('/config',express.static(__dirname + '/config'));
+
 // serve app
 app.use(express.static(__dirname + path));
 console.log('server started at port 3001');
